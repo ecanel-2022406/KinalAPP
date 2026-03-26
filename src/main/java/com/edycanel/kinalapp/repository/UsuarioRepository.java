7f566d85344e689afe94e477b0ad50a8949ae2f8
ecanel-2022406 <ecanel-2022406@kinal.edu.gt>
2026-03-26 00:29:34 -0600

    refactor: Se agregó el rpository de Usuarios ecanel-2022406

---
+++ src/main/java/com/edycanel/kinalapp/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.edycanel.kinalapp.repository;
+
+
+import com.edycanel.kinalapp.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+}
