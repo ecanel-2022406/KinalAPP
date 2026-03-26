cdbff8a593f40f62cbb00f35f2fc7cd746ca39a1
ecanel-2022406 <ecanel-2022406@kinal.edu.gt>
2026-03-25 23:31:13 -0600

    refactor: Se agregó una capa de servicio y una interfaz de servicio a Ventas ecanel-2022406

--- src/main/java/com/edycanel/kinalapp/repository/VentaRepository.java
+++ src/main/java/com/edycanel/kinalapp/repository/VentaRepository.java
@@ -3,5 +3,5 @@ package com.edycanel.kinalapp.repository;
 import com.edycanel.kinalapp.entity.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VentaRepository extends JpaRepository<Venta, String> {
+public interface VentaRepository extends JpaRepository<Venta, Integer> {
 }
